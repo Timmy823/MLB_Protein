ee871cc36fdcd023679c74479f9fbdb7c9064c64
Timmy <boss63081@gmail.com>
2017-12-27 01:41:29 +0800

    Update 12.26

--- basic.py
+++ basic.py
@@ -5,8 +5,8 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
-from sklearn.model_selection import cross_val_score
-
+from sklearn.cross_validation import KFold
+from sklearn.metrics import roc_auc_score
 with open('/home/mlb2017/res/phosphosite/train_data') as f:
     data = f.readlines()
 data = [x.strip() for x in data]
@@ -32,15 +32,40 @@ for seq in seqlist:
   freqlist.append(acids_count);
   acids_count = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0];
 ###print(freqlist)
-train_X = np.array(freqlist)
-train_Y = np.array(phoslist)
-clf = Pipeline([('scaler', StandardScaler()),
-                ('clf', SVC(C=float(sys.argv[1]),gamma=float(sys.argv[2])))])
-clf.fit(train_X, train_Y)
-scores = cross_val_score(clf,train_X,train_Y,cv=10,n_jobs=1)
-print('Test Accuracy: {}'.format(scores))
-print('CV accuracy: {:.3f} +/- {:.3f}'.format(np.mean(scores), np.std(scores)))
+freq = np.array(freqlist)
+X = np.load('/home/mlb2017/res/phosphosite/trainX.npy')
+X = np.hstack((freq, X))
+Y = np.array(phoslist)
+kf = KFold(len(X),n_folds=10)
+sum_train_score = 0
+sum_test_score = 0
+sum_auc1 = 0
+sum_auc2 = 0
 
-        
+for train_index,test_index in kf:
+  train_X,test_X = X[train_index],X[test_index]
+  train_Y,test_Y = Y[train_index],Y[test_index]
+  clf = Pipeline([('scaler', StandardScaler()),
+                ('clf', SVC(C=float(sys.argv[1]),gamma=float(sys.argv[2]),probability=True))])
+  clf.fit(train_X, train_Y)
+  y_score = clf.predict_proba(train_X)[:, 1]
+  y_decision = clf.decision_function(train_X)
+  sum_train_score = sum_train_score + clf.score(train_X,train_Y)
+  sum_test_score = sum_test_score + clf.score(test_X,test_Y)
+  print('Train Accuracy: {:.3f}'.format(clf.score(train_X,train_Y)))
+  print('Test Accuracy: {:.3f}'.format(clf.score(test_X,test_Y)))
+  Y_score = clf.predict_proba(train_X)[:, 1]
+  Y_decision = clf.decision_function(train_X)
+  sum_auc1 = sum_auc1 + roc_auc_score(test_Y, Y_score)
+  sum_auc2 = sum_auc2 + roc_auc_score(test_Y, Y_decision)
+  print('AUC: {:.3f}'.format(roc_auc_score(test_Y, Y_score)))
+  print('AUC(2): {:.3f}'.format(roc_auc_score(test_Y, Y_decision)))
+  print('---------------------------------------------------------------')
+print('Train Avg Accuracy: {:.3f}'.format(sum_train_score/10))
+print('Test Avg Accuracy: {:.3f}'.format(sum_test_score/10))
+print('AUC: {:.3f}'.format(sum_auc1/10))
+print('AUC(2): {:.3f}'.format(sum_auc2/10))
+
+
     
 
