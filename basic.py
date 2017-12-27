bbe76f280112b03c6ad916fed8a38ff1133050b5
Timmy <boss63081@gmail.com>
2017-12-27 09:46:55 +0800

    basic.py optimizes

--- basic.py
+++ basic.py
@@ -18,8 +18,8 @@ for site in data:
     seqlist.append(site[2])
 ###print(seqlist)
 acids = ["A","C","D","E","F","G","H","I","K","L","M","N","P","Q","R","S","T","V","W","Y"]
-freqlist = [];
-acids_count = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0];
+freqlist = []
+acids_count = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
 for seq in seqlist:
   temp = []
   for item in seq:
@@ -28,8 +28,8 @@ for seq in seqlist:
         acids_count[index] = acids_count[index] + 1
   for index in range(len(acids_count)):
     acids_count[index] = float(acids_count[index]/19)
-  freqlist.append(acids_count);
-  acids_count = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0];
+  freqlist.append(acids_count)
+  acids_count = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
 ###print(freqlist)
 freq = np.array(freqlist)
 X = np.load('/home/mlb2017/res/phosphosite/trainX.npy')
@@ -39,7 +39,7 @@ kf = KFold(len(X),n_folds=10,shuffle=True)
 sum_train_score = 0
 sum_test_score = 0
 sum_auc1 = 0
-sum_auc2 = 0
+###sum_auc2 = 0
 
 for train_index,test_index in kf:
   train_X,test_X = X[train_index],X[test_index]
@@ -52,16 +52,16 @@ for train_index,test_index in kf:
   print('Train Accuracy: {:.3f}'.format(clf.score(train_X,train_Y)))
   print('Test Accuracy: {:.3f}'.format(clf.score(test_X,test_Y)))
   Y_score = clf.predict_proba(test_X)[:, 1]  
-  Y_decision = clf.decision_function(test_X)
+  ###Y_decision = clf.decision_function(test_X)
   sum_auc1 = sum_auc1 + roc_auc_score(test_Y, Y_score)
-  sum_auc2 = sum_auc2 + roc_auc_score(test_Y, Y_decision)
+  ###sum_auc2 = sum_auc2 + roc_auc_score(test_Y, Y_decision)
   print('AUC: {:.3f}'.format(roc_auc_score(test_Y, Y_score)))
-  print('AUC(2): {:.3f}'.format(roc_auc_score(test_Y, Y_decision)))
+  ###print('AUC(2): {:.3f}'.format(roc_auc_score(test_Y, Y_decision)))
   print('---------------------------------------------------------------')
 print('Train Avg Accuracy: {:.3f}'.format(sum_train_score/10))
 print('Test Avg Accuracy: {:.3f}'.format(sum_test_score/10))
 print('AUC: {:.3f}'.format(sum_auc1/10))
-print('AUC(2): {:.3f}'.format(sum_auc2/10))
+###print('AUC(2): {:.3f}'.format(sum_auc2/10))
 
 
     
