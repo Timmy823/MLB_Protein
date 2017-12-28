695def85fe18c9593e5364fe037076bb8e4b5a40
Timmy <boss63081@gmail.com>
2017-12-28 11:47:15 +0800

    New method is appied.basic.py(351 featyres) and Freq.py(append freq feature) are added.

--- basic.py
+++ basic.py
@@ -3,7 +3,7 @@ import numpy as np
 import sklearn
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 from sklearn.svm import SVC
 from sklearn.cross_validation import KFold
 from sklearn.metrics import roc_auc_score
@@ -18,51 +18,56 @@ for site in data:
     seqlist.append(site[2])
 ###print(seqlist)
 acids = ["A","C","D","E","F","G","H","I","K","L","M","N","P","Q","R","S","T","V","W","Y"]
-freqlist = []
-acids_count = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
+hydro_index = [0.62, 0.29, -0.90, -0.74, 1.19, 0.48, -0.40, 1.38, -1.50, 1.06, 0.64, -0.78,0.12, -0.85, -2.53, -0.18, -0.05, 1.08, 0.81, 0.26]
+ACH_feature =[]
 for seq in seqlist:
+  ### ACH_feature
   temp = []
-  for item in seq:
-    for index in range(len(acids)):
-      if item == acids[index] :
-        acids_count[index] = acids_count[index] + 1
-  for index in range(len(acids_count)):
-    acids_count[index] = float(acids_count[index]/19)
-  freqlist.append(acids_count)
-  acids_count = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
-###print(freqlist)
-freq = np.array(freqlist)
+  for i in range(1,10,1):
+    hydro_index_count = 0.0
+    for j in range(9-i,10+i,1):        
+      hydro_index_count = hydro_index_count + hydro_index[acids.index(seq[j])]
+    avg = hydro_index_count/(2*i+1)
+    temp.append(round(avg,3))
+  ACH_feature.append(temp)
+ACH = np.array(ACH_feature)
 X = np.load('/home/mlb2017/res/phosphosite/trainX.npy')
-X = np.hstack((freq, X))
+X = np.hstack((ACH, X))
 Y = np.load('/home/mlb2017/res/phosphosite/trainY.npy')
 kf = KFold(len(X),n_folds=10,shuffle=True)
 sum_train_score = 0
 sum_test_score = 0
 sum_auc1 = 0
-###sum_auc2 = 0
-
+f = open('result.txt','a')
+f.write('python3 '+ sys.argv[1] +' '+sys.argv[2])
+f.write('/n')
 for train_index,test_index in kf:
   train_X,test_X = X[train_index],X[test_index]
   train_Y,test_Y = Y[train_index],Y[test_index]
   clf = Pipeline([('scaler', StandardScaler()),
-                ('clf', SVC(C=float(sys.argv[1]),gamma=float(sys.argv[2]),probability=True))])
+                  ('pca',PCA(n_components=100)),
+                  ('clf', SVC(C=float(sys.argv[1]),gamma=float(sys.argv[2]),probability=True))])
   clf.fit(train_X, train_Y)
   sum_train_score = sum_train_score + clf.score(train_X,train_Y)
   sum_test_score = sum_test_score + clf.score(test_X,test_Y)
   print('Train Accuracy: {:.3f}'.format(clf.score(train_X,train_Y)))
   print('Test Accuracy: {:.3f}'.format(clf.score(test_X,test_Y)))
   Y_score = clf.predict_proba(test_X)[:, 1]  
-  ###Y_decision = clf.decision_function(test_X)
   sum_auc1 = sum_auc1 + roc_auc_score(test_Y, Y_score)
-  ###sum_auc2 = sum_auc2 + roc_auc_score(test_Y, Y_decision)
   print('AUC: {:.3f}'.format(roc_auc_score(test_Y, Y_score)))
-  ###print('AUC(2): {:.3f}'.format(roc_auc_score(test_Y, Y_decision)))
+  f.write('Train Accuracy: {:.3f}'.format(clf.score(train_X,train_Y)))
+  f.write('/n')
+  f.write('Test Accuracy: {:.3f}'.format(clf.score(test_X,test_Y)))
+  f.write('/n')
+  f.write('AUC: {:.3f}'.format(roc_auc_score(test_Y, Y_score)))
+  f.write('/n')
   print('---------------------------------------------------------------')
 print('Train Avg Accuracy: {:.3f}'.format(sum_train_score/10))
 print('Test Avg Accuracy: {:.3f}'.format(sum_test_score/10))
 print('AUC: {:.3f}'.format(sum_auc1/10))
-###print('AUC(2): {:.3f}'.format(sum_auc2/10))
-
-
-    
-
+f.write('Train Avg Accuracy: {:.3f}'.format(sum_train_score/10))
+f.write('/n')
+f.write('Test Avg Accuracy: {:.3f}'.format(sum_test_score/10))
+f.write('/n')
+f.write('AUC: {:.3f}'.format(sum_auc1/10))
+f.write('/n')
